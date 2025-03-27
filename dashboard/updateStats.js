451bdd1bf27dacbc4b8b27272ca994d945a5ec91
jparras <jparras1@my.bcit.ca>
2025-03-27 14:42:08 -0700

    fix typo2

--- dashboard/updateStats.js
+++ dashboard/updateStats.js
@@ -37,7 +37,7 @@ const getStats = () => {
     makeReq(ANALYZER_API_URL.stats, (statsResult) => {
         updateCodeDiv("analyzer-stats", statsResult)
 
-        // get the last index for each event
+        // get the last index for each events
         // const lastParkIndex = Math.max(statsResult.num_park_car - 1, 0)
         // const lastReserveIndex = Math.max(statsResult.num_reserve_car - 1, 0)
         const randomParkIndex = Math.floor(Math.random() * (Math.max(statsResult.num_park_car - 1, 0)))
