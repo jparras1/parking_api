f8b4ee63edb809848c52ec1ca6a8eebcded2701f
jparras <jparras1@my.bcit.ca>
2025-03-27 15:22:07 -0700

    remove random events

--- dashboard/updateStats.js
+++ dashboard/updateStats.js
@@ -43,8 +43,6 @@ const getStats = () => {
         // get the last index for each event
         const lastParkIndex = Math.max(statsResult.num_park_car - 1, 0)
         const lastReserveIndex = Math.max(statsResult.num_reserve_car - 1, 0)
-        // const randomParkIndex = Math.floor(Math.random() * (Math.max(statsResult.num_park_car - 1, 0)))
-        // const randomReserveIndex = Math.floor(Math.random() * (Math.max(statsResult.num_reserve_car - 1, 0)))
 
         makeReq(ANALYZER_API_URL.park(0), (result) => {
             updateCodeDiv("first-event-park",
