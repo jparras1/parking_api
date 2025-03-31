8a1139623b579c3c5bd3517a70e27249f9383641
jparras <jparras1@my.bcit.ca>
2025-03-30 19:19:14 -0700

    push all files

---
+++ storage/create_db.py
@@ -0,0 +1,22 @@
+"""creates database"""
+import sys
+from models import Base
+from db import engine_db
+
+def create_tables():
+    """create db tables"""
+    Base.metadata.create_all(engine_db)
+
+def drop_tables():
+    """delete db tables"""
+    Base.metadata.drop_all(engine_db)
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1 and sys.argv[1] == "drop":
+        drop_tables()
+    create_tables()
+
+
+# In the terminal, you can now use:
+# python create_db.py => creates all tables
+# python create_db.py drop => drops all tables
